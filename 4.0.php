186026ac1430086d98a8b1e494fb180c274ee396
MaxSukhoterin <161084415+MaxSukhoterin@users.noreply.github.com>
2024-11-10 21:32:12 +0200

    Add files via upload

---
+++ 4.0.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="4.0.css">
+<?php
+echo "<h1>Дякуємо за довіру, " . $_POST["Surname"] . " " . $_POST["Name"] . " " . $_POST["Thirdname"] . "!</h1>";
+
+echo "<h2>Ви народилися: " . $_POST["Date"] . ", " . "у місті " . $_POST["State"] . "</h2>";
